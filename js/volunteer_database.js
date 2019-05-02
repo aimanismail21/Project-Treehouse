c54f02181b1b3e283ca832ad42a943d69e346847
Tommy <twmay98@hotmail.com>
2019-05-02 10:50:13 -0700

    Changed radio buttons to select with options. made dropdown better so you can click each criteria and select 1 from the list that shows up made update_by_address function

--- js/volunteer_database.js
+++ js/volunteer_database.js
@@ -46,7 +46,6 @@ function dropdown(id){
 
 // function to sort volunteer list to only show ones with entered criterion
 function update_by_criteria() {
-
     // clears the volunteers list
     document.getElementById('volunteers').innerHTML = "";
     let ref = firebase.database().ref("Volunteers/");
@@ -55,40 +54,35 @@ function update_by_criteria() {
         function(snap){
 
             snap.forEach(function(snap){
-                // initialize selections to value in database
-                let city_selection = snap.child("City").val();
-                let pets_selection = snap.child("Pets").val();
-                let family_selection = snap.child("HouseHoldMembers").val()
+                let city_selection = null;
+                let pets_selection = null;
+                let family_selection = null;
 
-                //takes input that was entered into criteria form
-                let radios = document.getElementsByName('city');
-                for (let i = 0, length = radios.length; i < length; i++)
-                {
-                    if (radios[i].checked)
-                    {
-                        city_selection = radios[i].value;
-                        break;
-                    }
+                // initialize city to value in database if nothing is selected
+                if(document.getElementById('city').value === '') {
+                    city_selection = snap.child("City").val();
+                }
+                // if a city is selected take its value
+                else{
+                    city_selection = document.getElementById('city').value
                 }
 
-                radios = document.getElementsByName('pets');
-                for (let i = 0, length = radios.length; i < length; i++)
-                {
-                    if (radios[i].checked)
-                    {
-                        pets_selection = radios[i].value;
-                        break;
-                    }
+                // initialize pets to value in database if nothing is selected
+                if(document.getElementById('pets').value === '') {
+                    pets_selection = snap.child("Pets").val();
+                }
+                // if pets is selected take its value
+                else{
+                    pets_selection = document.getElementById('pets').value
                 }
 
-                radios = document.getElementsByName('family');
-                for (let i = 0, length = radios.length; i < length; i++)
-                {
-                    if (radios[i].checked)
-                    {
-                        family_selection = radios[i].value;
-                        break;
-                    }
+                // initialize family to value in database if nothing is selected
+                if(document.getElementById('family').value === '') {
+                    family_selection = snap.child("HouseHoldMembers").val();
+                }
+                // if family is selected take its value
+                else{
+                    family_selection = document.getElementById('family').value
                 }
 
                 // obtains info for each volunteer from database
@@ -127,5 +121,44 @@ function update_by_criteria() {
 
 // function to sort volunteer list to only show ones with entered address
 function update_by_address() {
-    let address = document.getElementById("volunteers").elements[0].value;
+    document.getElementById('volunteers').innerHTML = "";
+    let ref = firebase.database().ref("Volunteers/");
+    ref.on(
+        "value",
+        function(snap){
+            snap.forEach(function(snap){
+                // get address entered to search for
+                let address_entered = document.getElementById("address").value;
+
+                // only write volunteer with entered address
+                if(address_entered === snap.child("Address").val()) {
+                    // obtains info for each volunteer from database
+                    let first_name = snap.child("FirstName").val();
+                    let last_name = snap.child("LastName").val();
+                    let city = snap.child("City").val();
+                    let address = snap.child("Address").val();
+                    let phone_number = snap.child("PhoneNumber").val();
+                    let email = snap.child("Email").val();
+                    let household_members = snap.child("HouseHoldMembers").val();
+                    let pets = snap.child("Pets").val();
+                    let type_of_room = snap.child("TypeOfRoom").val();
+                    let availability = snap.child("Availability").val();
+
+                    // create new volunteer listing element
+                    let volunteer_listing = document.createElement("div");
+                    volunteer_listing.setAttribute("class", "volunteer_info");
+                    volunteer_listing.style.border = '2px solid black';
+                    volunteer_listing.innerHTML = first_name + '<br>' + last_name + '<br>' + city + '<br>' + address + '<br>' +
+                        phone_number + '<br>' + household_members + '<br>' + 'pets: ' + pets + '<br>' +
+                        type_of_room + '<br>' + availability + '<br>' + email + '<br>';
+                    volunteer_listing.onclick = function () {
+                        // - go to volunteers profile
+                    };
+
+                    // append volunteer listing to volunteers list
+                    document.getElementById('volunteers').appendChild(volunteer_listing);
+                }
+            });
+
+        });
 }
