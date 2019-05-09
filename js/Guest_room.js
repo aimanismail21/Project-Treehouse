e46f78c1e819696ca440402c88f370910521b3fc
Tommy <twmay98@hotmail.com>
2019-05-09 10:07:51 -0700

    refactoring / fixing and adding comments

--- js/Guest_room.js
+++ js/Guest_room.js
@@ -1,51 +1,43 @@
 // takes all database profile data to display on profile page
 var uid = null;
 var displayName = null;
-// initialize page elements
+
+// initialize variable
+let is_social_worker = null;
+let is_volunteer = null;
+
+// initialize page
 initApp = function () {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
             displayName = user.displayName;
             uid = user.uid;
-            let dbref = firebase.database().ref("Volunteers/"+uid);
+            let dbref = firebase.database().ref("Users/" + uid);
             dbref.on('value', (snapshot) => {
-                document.getElementById("first_name").value = snapshot.child('FirstName').val();
-                document.getElementById("last_name").value = snapshot.child('LastName').val();
-                document.getElementById("city").value = snapshot.child('City').val();
-                document.getElementById("pets").value = snapshot.child('Pets').val();
-                document.getElementById("family").value = snapshot.child('HouseHoldMembers').val();
-                document.getElementById("roomtype").value = snapshot.child('TypeOfRoom').val();
-                document.getElementById("address").value = snapshot.child('Address').val();
-                document.getElementById("phone_number").value = snapshot.child('PhoneNumber').val();
-                let is_social_worker = snapshot.child('IsSocialWorker').val();
-                let is_volunteer = snapshot.child('IsVolunteer').val();
+                // initialize page elements with data from database
+                initialize_info(snapshot);
 
-                // only show elements for social workers / volunteers that they should be able to see
-                if(is_social_worker){
-                    document.getElementById('guest_room').style.display = 'none';
-                    document.getElementById('query_menu').style.display = 'block';
-                }
-                if(is_volunteer){
-                    document.getElementById('query_menu').style.display = 'none';
-                    document.getElementById('guest_room').style.display = 'block';
-                }
+                // initialize elements user should be able to see
+                initialize_elements(snapshot);
             });
 
         } else {
-
+            // do nothing
         }
     }, function (error) {
         console.log(error);
     });
 };
 
+
 window.addEventListener('load', function () {
     initApp()
 });
 
+
 // update info of volunteer guest room in database
-function update_info(){
+function update_info() {
     let city = document.getElementById("city").value;
     let pets = document.getElementById("pets").value;
     let family = document.getElementById("family").value;
@@ -55,8 +47,8 @@ function update_info(){
     let address = document.getElementById("address").value;
     let phone_number = document.getElementById("phone_number").value;
 
-    // change Uid2 to uid that is taken from logged in user
-    let dbref = firebase.database().ref("Volunteers/"+uid);
+    // sets entered info to associated user id in database
+    let dbref = firebase.database().ref("Users/" + uid);
     dbref.update({
         FirstName: first_name,
         LastName: last_name,
@@ -70,31 +62,34 @@ function update_info(){
     document.getElementById('confirmation').innerHTML = 'info has been written'
 }
 
-// resets into to what is currently in the database
-function reset_info(){
+
+// resets info to what is currently in the database
+function reset_info() {
     initApp();
     document.getElementById('confirmation').innerHTML = 'info has been reset';
 }
 
+
 // sets room availability in database given no or yes
-function set_room_availability(availability){
-    let dbref = firebase.database().ref("Volunteers/" + uid);
-    
+function set_room_availability(availability) {
+    let dbref = firebase.database().ref("Users/" + uid);
+
     // if availability = yes then availability is set to open in database
-    if(availability === 'yes'){
+    if (availability === 'yes') {
         dbref.update({
             Availability: 'Open'
         });
         document.getElementById('confirmation').innerHTML = 'Room set to Available'
     }
     // if availability = no then availability is set to closed in database
-    else if(availability === 'no'){
+    else if (availability === 'no') {
         dbref.update({
             Availability: 'Closed'
         });
         document.getElementById('confirmation').innerHTML = 'Room set to Not Available'
     }
 }
+
 
 // submit house image to storage
 function submit_image() {
@@ -105,4 +100,34 @@ function submit_image() {
     });
     var storageRef = firebase.storage().ref(uid + '/' + new_file.name);
     storageRef.put(new_file);
+}
+
+
+// initialize page elements with data from database
+function initialize_info(snapshot) {
+    document.getElementById("first_name").value = snapshot.child('FirstName').val();
+    document.getElementById("last_name").value = snapshot.child('LastName').val();
+    document.getElementById("city").value = snapshot.child('City').val();
+    document.getElementById("pets").value = snapshot.child('Pets').val();
+    document.getElementById("family").value = snapshot.child('HouseHoldMembers').val();
+    document.getElementById("roomtype").value = snapshot.child('TypeOfRoom').val();
+    document.getElementById("address").value = snapshot.child('Address').val();
+    document.getElementById("phone_number").value = snapshot.child('PhoneNumber').val();
+}
+
+
+function initialize_elements(snapshot) {
+    // get info on user type
+    is_social_worker = snapshot.child('IsSocialWorker').val();
+    is_volunteer = snapshot.child('IsVolunteer').val();
+
+    // only show elements for social workers / volunteers that they should be able to see
+    if (is_social_worker) {
+        document.getElementById('guest_room').style.display = 'none';
+        document.getElementById('query_menu').style.display = 'block';
+    }
+    if (is_volunteer) {
+        document.getElementById('query_menu').style.display = 'none';
+        document.getElementById('guest_room').style.display = 'block';
+    }
 }
