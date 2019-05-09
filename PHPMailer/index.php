d13db24cde97380c52d2d1eade0784a21de451b0
Brittany Lo <lBrittany@my.bcit.ca>
2019-05-09 10:29:08 -0700

    commit for PHP files

---
+++ PHPMailer/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'project_treehouse@yahoo.com';                     // SMTP username
+    $mail->Password   = "bcit1234";                               // SMTP password
+    $mail->SMTPSecure = 'tls';                          // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('project_treehouse@yahoo.com', 'Mailer');
+    $mail->addAddress('project_treehouse@yahoo.com', 'Joe User');     // Add a recipient
+    $mail->addReplyTo('project_treehouse@yahoo.com', 'Information');
+
+
+    $body = " i like pineapples ";
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Test';
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+
+<html lang="en">
+
+inset things
+</html>
