527dcccb3bbc8b6312d8bd3b56154a0dc9a471d2
Tommy <twmay98@hotmail.com>
2019-05-10 13:19:08 -0700

    added onclick deletion of nyan cats in easter egg

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
+let catImageArray = [];
+let audio = null;
+let catsAlive = 3;
+
 // Side Bar
-(function($) {
+(function ($) {
     "use strict";
 
     // Toggle the side navigation
-    $("#sidebarToggle").on('click', function(e) {
+    $("#sidebarToggle").on('click', function (e) {
         e.preventDefault();
         $("body").toggleClass("sidebar-toggled");
         $(".sidebar").toggleClass("toggled");
     });
 
     // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
         if ($(window).width() > 768) {
             var e0 = e.originalEvent,
                 delta = e0.wheelDelta || -e0.detail;
@@ -20,7 +24,7 @@
     });
 
     // Scroll to top button appear
-    $(document).on('scroll', function() {
+    $(document).on('scroll', function () {
         var scrollDistance = $(this).scrollTop();
         if (scrollDistance > 100) {
             $('.scroll-to-top').fadeIn();
@@ -30,7 +34,7 @@
     });
 
     // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(event) {
+    $(document).on('click', 'a.scroll-to-top', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top)
@@ -42,54 +46,64 @@
 
 //Easter Egg Script
 var counter = 0;
-nyan_cat_attack = document.getElementById('easter_egg');
-nyan_cat_attack.addEventListener('click', function(){
+let nyan_cat_attack = document.getElementById('easter_egg');
+nyan_cat_attack.addEventListener('click', function () {
+
     counter += 1;
-    if(counter === 3) {
-        function image(src) {
+    if (counter === 3) {
+        function image(src, id) {
             this.im = document.createElement('img');
             this.im.src = src;
+            this.im.id = 'cat' + id;
             document.body.appendChild(this.im);
             this.im.style.position = 'absolute';
-            this.setTop = function (top) {
-                this.im.style.top = top
-            };
-            this.setRandomLocation = function () {
-                this.im.style.top = Math.random() * (window.innerHeight - 200);
-                this.im.style.left = Math.random() * (window.innerWidth - 200)
-            };
 
-            this.animate = function() {
+            this.animate = function () {
                 setInterval(() => {
-                    $(this.im).animate({left: Math.random() * (window.innerWidth - 50),
+                    $(this.im).animate({
+                        left: Math.random() * (window.innerWidth - 50),
                         top: Math.random() * (window.innerHeight - 50),
                         height: '150px',
                     }, 'slow')
-                }, 800);
+                }, 2000);
 
             }
         }
-        let imageArray = [];
-        for (i = 0; i < 1; i++) {
-            imageArray.push(new image('images/nyancat.gif'));
-            imageArray[i].animate()
+
+        for (let i = 0; i < 3; i++) {
+            catImageArray[i] = new image('images/nyancat.gif', i);
+            catImageArray[i].im.onclick = function () {
+                catImageArray[i].im.style.display = 'none';
+                catsAlive--;
+                checkCatsAlive();
+            };
+            catImageArray[i].animate();
         }
-        var audio = new Audio ('sound/8_bit_NYAN.mp3');
+        audio = new Audio('sound/8_bit_NYAN.mp3');
         audio.play();
     }
 });
 
+
+// checks if all cats have been clicked and stops audio if they have
+function checkCatsAlive(){
+    if (catsAlive === 0){
+        console.log('cats have died');
+        audio.pause();
+    }
+}
+
 // Sidenav Content Script
-$(function(){
-    $("#menu-toggle").click(function(e) {
+$(function () {
+    $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
 
-    $(window).resize(function(e) {
-        if($(window).width()<=768){
+    $(window).resize(function (e) {
+        if ($(window).width() <= 768) {
             $("#wrapper").removeClass("toggled");
-        }else{
+        } else {
             $("#wrapper").addClass("toggled");
         }
     });
