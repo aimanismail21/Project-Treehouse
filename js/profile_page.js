3a77e3c0895da902d9419b382a19cf0b91cc57d8
Tommy <twmay98@hotmail.com>
2019-05-13 13:58:39 -0700

    phone number input field automatically adds dashes so user doesnt need to doesnt allow over 10 numbers doesnt allow letters

--- js/profile_page.js
+++ js/profile_page.js
@@ -70,7 +70,7 @@ function update_profile_info() {
     first_name = document.getElementById("first_name").value;
     last_name = document.getElementById("last_name").value;
     address = document.getElementById("address").value;
-    phone_number = document.getElementById("phone_number").value;
+    phone_number = document.getElementById("phone").value;
 
     // if all form elements are valid update user info
     if (validate_inputs()) {
@@ -104,7 +104,7 @@ function initialize_info(snapshot) {
     document.getElementById("last_name").value = snapshot.child('LastName').val();
     document.getElementById("city").value = snapshot.child('City').val();
     document.getElementById("address").value = snapshot.child('Address').val();
-    document.getElementById("phone_number").value = snapshot.child('PhoneNumber').val();
+    document.getElementById("phone").value = snapshot.child('PhoneNumber').val();
 }
 
 
@@ -148,7 +148,7 @@ function validate_inputs() {
 
     if (phone_match === null) {
         window.alert("Please enter a correct phone number eg: 999-999-9999.");
-        document.getElementById('phone_number').focus();
+        document.getElementById('phone').focus();
         return false;
     }
 
@@ -200,3 +200,102 @@ function validate_inputs() {
     });
 
 })(jQuery); // End of use strict
+
+
+// testing
+
+function phone_formatting(ele,restore) {
+    var new_number,
+        selection_start = ele.selectionStart,
+        selection_end = ele.selectionEnd,
+        number = ele.value.replace(/\D/g,'');
+
+    // automatically add dashes
+    if (number.length > 2) {
+        // matches: 123 || 123-4 || 123-45
+        new_number = number.substring(0,3) + '-';
+        if (number.length === 4 || number.length === 5) {
+            // matches: 123-4 || 123-45
+            new_number += number.substr(3);
+        }
+        else if (number.length > 5) {
+            // matches: 123-456 || 123-456-7 || 123-456-789
+            new_number += number.substring(3,6) + '-';
+        }
+        if (number.length > 6) {
+            // matches: 123-456-7 || 123-456-789 || 123-456-7890
+            new_number += number.substring(6);
+        }
+    }
+    else {
+        new_number = number;
+    }
+
+    // if value is higher than 12, last number is dropped
+    // if inserting a number before the last character, numbers
+    // are shifted right, only 12 characters will show
+    ele.value =  (new_number.length > 12) ? new_number.substring(0,12) : new_number;
+
+    // restore cursor selection,
+    // prevent it from going to the end
+    // UNLESS
+    // cursor was at the end AND a dash was added
+    document.getElementById('msg').innerHTML='<p>Selection is: ' + selection_end + ' and length is: ' + new_number.length + '</p>';
+
+    if (new_number.slice(-1) === '-' && restore === false
+        && (new_number.length === 8 && selection_end === 7)
+        || (new_number.length === 4 && selection_end === 3)) {
+        selection_start = new_number.length;
+        selection_end = new_number.length;
+    }
+    else if (restore === 'revert') {
+        selection_start--;
+        selection_end--;
+    }
+    ele.setSelectionRange(selection_start, selection_end);
+
+}
+
+function phone_number_check(field,e) {
+    var key_code = e.keyCode,
+        key_string = String.fromCharCode(key_code),
+        press_delete = false,
+        dash_key = 189,
+        delete_key = [8,46],
+        direction_key = [33,34,35,36,37,38,39,40],
+        selection_end = field.selectionEnd;
+
+    // delete key was pressed
+    if (delete_key.indexOf(key_code) > -1) {
+        press_delete = true;
+    }
+
+    // only force formatting is a number or delete key was pressed
+    if (key_string.match(/^\d+$/) || press_delete) {
+        phone_formatting(field,press_delete);
+    }
+    // do nothing for direction keys, keep their default actions
+    else if(direction_key.indexOf(key_code) > -1) {
+        // do nothing
+    }
+    else if(dash_key === key_code) {
+        if (selection_end === field.value.length) {
+            field.value = field.value.slice(0,-1)
+        }
+        else {
+            field.value = field.value.substring(0,(selection_end - 1)) + field.value.substr(selection_end)
+            field.selectionEnd = selection_end - 1;
+        }
+    }
+    // all other non numerical key presses, remove their value
+    else {
+        e.preventDefault();
+//    field.value = field.value.replace(/[^0-9\-]/g,'')
+        phone_formatting(field,'revert');
+    }
+
+}
+
+document.getElementById('phone').onkeyup = function(e) {
+    phone_number_check(this,e);
+};
