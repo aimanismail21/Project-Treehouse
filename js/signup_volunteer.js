7307627501d79c4e7e2f4ed0b3439a71147d1976
Aiman Ismail <aimanismail15@gmail.com>
2019-05-21 09:38:09 -0700

    Updated URL of signup_volutneer.js Added fixed border class to bottom nav on signup_and_login.html

--- js/signup_volunteer.js
+++ js/signup_volunteer.js
@@ -17,7 +17,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'profile.html',
+    signInSuccessUrl: 'dashboard_profile.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
