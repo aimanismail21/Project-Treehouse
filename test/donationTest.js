3afcc84980e2c4409f4696f02cc1b558b429a38b
John Gilpin <johnnyrgilpin@gmail.com>
2019-05-21 12:02:58 -0700

    tested place_amount: success; renamed getAmount to get_amount

--- test/donationTest.js
+++ test/donationTest.js
@@ -1,8 +1,25 @@
-TestCase("getAmount testTest", {
+TestCase("get_amount testTest", {
     "test getAmount amount is obj value": function(){
         let objButton = document.createElement("input");
         objButton.value = "1";
-        getAmount(objButton);
+        get_amount(objButton);
         assertEquals("1", amount);
     }
 });
+
+
+TestCase("place_amount testTest", {
+    "test place_amount value is amount": function(){
+        let some_element = document.createElement("input");
+        place_amount(some_element);
+        let value = some_element.value;
+        assertEquals(amount, value);
+    },
+    "test place_amount placeholder is amount": function(){
+        let some_element = document.createElement("input");
+        place_amount(some_element);
+        let placeholder = some_element.placeholder;
+        assertEquals(amount, placeholder);
+    }
+});
+
