26ad3b7f236b8732dcd5241ac685fb62228a83fb
Jilliane Vina <jvina@my.bcit.ca>
2019-05-22 12:37:30 -0700

    removed whitespace.

--- js/forgot_password.js
+++ js/forgot_password.js
@@ -1,16 +1,19 @@
-//Reset password link is sent to email entered by user in forgot_password.html input box
+// Reset password link is sent to email entered by user
 function is_valid(){
     var email = document.getElementById("example_input_email1");
     if (!reg_exp(email.value)){
         alert("Invalid Email!");
-        return;
+        return false;
     }
     else {
         forgot_password();
         window.location.href = "confirmation_forget_password.html";
+        return true;
     }
 }
 
+
+// Checks if email entered is valid
 function reg_exp(valid_email){
     var email_regex =/^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+.([A-Za-z]{2,})$/;
     return email_regex.test(valid_email);
