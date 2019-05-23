7e051b515635d3980cc8459886f7da0e903148c8
John Gilpin <johnnyrgilpin@gmail.com>
2019-05-23 02:00:37 -0700

    fixed a stripe function

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const logging = require('@google-cloud/logging')();
 const stripe = require('stripe')(functions.config().stripe.token);
-const currency = functions.config().stripe.currency || selected_currency;
+const currency = functions.config().stripe.currency || "CAD";
 
 // [START chargecustomer]
 // Charge the Stripe customer whenever an amount is written to the Realtime database
